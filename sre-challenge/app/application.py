bc59f8485d875434ba19d0550a5d5294c30047b8
Victor <victorpayasanchez@gmail.com>
2026-09-17 11:58:53 +0200

    Initial commit

---
+++ sre-challenge/app/application.py
@@ -0,0 +1,67 @@
+import sqlite3
+import logging
+import os
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask import Flask, session, redirect, url_for, request, render_template, abort
+
+
+app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY')
+app.logger.setLevel(logging.INFO)
+
+def get_db_connection():
+    connection = sqlite3.connect("database.db")
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
+def is_authenticated():
+    if "username" in session:
+        return True
+    return False
+
+
+def authenticate(username, password):
+    connection = get_db_connection()
+    users = connection.execute("SELECT * FROM users").fetchall()
+    connection.close()
+
+    for user in users:
+        if user["username"] == username and check_password_hash(user["password"], password):
+            app.logger.info(f"the user '{username}' logged in successfully")
+            session["username"] = username
+            return True
+
+    app.logger.warning(f"the user '{ username }' failed to log in")
+    abort(401)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", is_authenticated=is_authenticated())
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # print(generate_password_hash("supersecret"))
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if authenticate(username, password):
+            return redirect(url_for("index"))
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("index"))
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}, 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
